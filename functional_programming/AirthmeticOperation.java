91cb833f0fe41662a08e7a92f2329d728a702164
Gautam Borkar <gborkar@us.ibm.com>
2025-10-07 17:46:09 -0400

    Example for functional interface.

---
+++ functional_programming/AirthmeticOperation.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface AirthmeticOperation {
+  long operation(int a, int b);
+}
---
+++ functional_programming/AirthmeticOperationImpl.java
@@ -0,0 +1,8 @@
+public class AirthmeticOperationImpl {
+
+  public static void main(String[] args) {
+    AirthmeticOperation add = (a, b) -> a + b;
+    add.operation(10, 11);
+  }
+
+}
