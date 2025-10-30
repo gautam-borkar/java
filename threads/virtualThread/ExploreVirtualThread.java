fc79016b348eeb91f53024db69c8db399bad705c
Raghoba Gautam Borkar <gautam_borkar@mymail.com>
2025-10-30 11:17:55 -0400

    Create virtual thread

--- threads/virtualThread/ExploreVirtualThread.java
+++ threads/virtualThread/ExploreVirtualThread.java
@@ -1,6 +1,25 @@
 package virtualThread;
 
-public class ExploreVirtualThread {
+import java.lang.Thread.Builder.OfVirtual;
 
+public class ExploreVirtualThread {
+    private static void doSomeWork() {
+        try {
+            System.out.println("Started doSomeWork");
+            Thread.sleep(1000);
+            System.out.println("Ended doSomeWork");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void main() throws InterruptedException {
+        OfVirtual virtualThread1 = Thread.ofVirtual().name("virtualThread-1");
+        virtualThread1.start(() -> System.out.println("Run task virtualThread-1 in background"));
+
+        Thread.ofVirtual().name("virtualThread-2").start(ExploreVirtualThread::doSomeWork);
+
+        System.out.println("Main program ended");
+    }
     
 }
