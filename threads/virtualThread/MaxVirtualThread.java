2ce0382765de6f07ed232db1f2043bfebb4542b0
Raghoba Gautam Borkar <gautam_borkar@mymail.com>
2025-10-30 11:50:28 -0400

    Added statement to print no of cores.

--- threads/virtualThread/MaxVirtualThread.java
+++ threads/virtualThread/MaxVirtualThread.java
@@ -5,17 +5,20 @@ import java.util.stream.IntStream;
 public class MaxVirtualThread {
 
     public static void main(String[] args) throws InterruptedException {
-        int MAX_THREAD = 10000000;
+        System.out.println("Number of cores: " + Runtime.getRuntime().availableProcessors());
+
+        int MAX_THREAD = 10;
         IntStream.rangeClosed(1, MAX_THREAD).forEach((i) -> Thread.ofVirtual().name("virtual-thread-" + i).start(() -> {
             try {
-                System.out.println("Started thread: " + i);
+                System.out.println("[" + Thread.currentThread() + "] Started thread: " + i);
                 Thread.sleep(5000);
+                System.out.println("[" + Thread.currentThread() + "] Ended thread: " + i);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }));
 
-        Thread.sleep(10000000);
+        Thread.sleep(10000);
     }
 
 }
